819770cbe8f4274f594786e5f334f618d3dfed34
nurmil <nurmil@thoughtworks.com>
2017-04-26 12:24:00 +0530

    [000] + Urmil/Parvez | Refactor : Remove commented code.

--- tests/test_main.lua
+++ tests/test_main.lua
@@ -1,21 +1,6 @@
 luaunit = require('./lib/luaunit/luaunit')
 main = require('./src/packet/Read_Packet')
 
---function testAddPositive()
---    luaunit.assertEquals(add(1,1),2)
---    print("testAddPositive passed")
---end
---
---function testAddZero()
---    luaunit.assertEquals(add(1,0),0)
---    luaunit.assertEquals(add(0,5),0)
---    luaunit.assertEquals(add(0,0),0)
---    print("testAddZero passed")
---end
---
---testAddPositive()
---testAddZero()
-
 function should_read_packet_for_one_service_and_zero_characteristic()
   binary_packet = {
     1, 3, 1, 1, 1,  --header
