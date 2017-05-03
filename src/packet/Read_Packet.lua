4543c9cad9a97ccf9046fa1172c7a75ea330125a
nurmil <nurmil@thoughtworks.com>
2017-05-03 09:35:12 +0530

    [000] + Urmil/Varun | Using Service ID with start index 1 since Lua indexes tables/arrays from 1. Hence, no mapping required from 1 to 0. Fixed tests.

--- src/packet/Read_Packet.lua
+++ src/packet/Read_Packet.lua
@@ -1,36 +1,36 @@
 -- Keeping uninitialized/default values of service_id and characteristic_count as -1
 
 -- GLOBAL VARIABLES
-SERVICE_ID_INDEX = 5+1
-CHARACTERISTICS_COUNT_INDEX = 6+1
-CHARACTERISTICS_BUFFER_START_INDEX = 7+1
+SERVICE_ID_INDEX = 6 -- INDICES IN LUA START FROM 1
+CHARACTERISTICS_COUNT_INDEX = 7 -- DO I NEED TO REPEAT, IT STARTS FROM 1
+CHARACTERISTICS_BUFFER_START_INDEX = 8 -- TRY AND TEST YOURSELF. It's TRUE
 
 -- Meta class
-Read_Packet = {service_id = -1, characteristic_count = -1, characteristics_ids = {}}
+Read_Packet = { service_id = -1, characteristic_count = -1, characteristics_ids = {} }
 
 -- Derived class method new
 
-function Read_Packet:new (o,service_id,characteristic_count, characteristics_ids)
-   o = o or {}
-   setmetatable(o, self)
-   self.__index = self
-   self.service_id = service_id or -1	
-   self.characteristic_count = characteristic_count or -1
-   self.characteristics_ids = characteristics_ids or {}
-   return o
+function Read_Packet:new(o, service_id, characteristic_count, characteristics_ids)
+    o = o or {}
+    setmetatable(o, self)
+    self.__index = self
+    self.service_id = service_id or -1
+    self.characteristic_count = characteristic_count or -1
+    self.characteristics_ids = characteristics_ids or {}
+    return o
 end
 
 -- Derived class method parse_read		
 
-function parse_read (binary_packet)
-  
-  service_id = binary_packet[SERVICE_ID_INDEX]-1;
-  characteristic_count = binary_packet[CHARACTERISTICS_COUNT_INDEX];
-  characteristics_ids = {}
-  
-  for i=1, characteristic_count do
-  	characteristics_ids[i] = binary_packet[CHARACTERISTICS_BUFFER_START_INDEX + i - 1]
-  end	
+function parse_read(binary_packet)
 
-  return Read_Packet:new{service_id=service_id, characteristic_count=characteristic_count, characteristics_ids=characteristics_ids}
+    service_id = binary_packet[SERVICE_ID_INDEX];
+    characteristic_count = binary_packet[CHARACTERISTICS_COUNT_INDEX];
+    characteristics_ids = {}
+
+    for i = 1, characteristic_count do
+        characteristics_ids[i] = binary_packet[CHARACTERISTICS_BUFFER_START_INDEX + i - 1]
+    end
+
+    return Read_Packet:new { service_id = service_id, characteristic_count = characteristic_count, characteristics_ids = characteristics_ids }
 end
--- tests/packet/packet_test.lua
+++ tests/packet/packet_test.lua
@@ -2,58 +2,60 @@ luaunit = require('./lib/luaunit/luaunit')
 main = require('./src/packet/Read_Packet')
 
 function should_read_packet_for_one_service_and_zero_characteristic()
-  binary_packet = {
-    1, 3, 1, 1, 1,  --header
-    1,              --service_id
-    0               --char_count
-  }
-  read_packet = parse_read(binary_packet)
-  luaunit.assertEquals(read_packet.service_id,0)
-  luaunit.assertEquals(read_packet.characteristic_count,0)
-  print("should_read_packet_for_one_service_and_zero_characteristic passed")
+    binary_packet = {
+        1, 3, 1, 1, 1, --header
+        1, --service_id
+        0 --char_count
+    }
+    read_packet = parse_read(binary_packet)
+    luaunit.assertEquals(read_packet.service_id, 1)
+    luaunit.assertEquals(read_packet.characteristic_count, 0)
+    print("should_read_packet_for_one_service_and_zero_characteristic passed")
 end
+
 should_read_packet_for_one_service_and_zero_characteristic()
 
 function should_read_packet_for_one_service_and_one_characteristic()
-  binary_packet = {
-    1, 3, 1, 1, 1,  --header
-    1,              --service_id
-    1, 1            --char_count,char_id
-  }
-  
-  read_packet = parse_read(binary_packet)
-  luaunit.assertEquals(read_packet.service_id,0)
-  luaunit.assertEquals(read_packet.characteristic_count,1)
-  
-  characteristics_ids = {1};
-  
-  for i = 1, read_packet.characteristic_count do
-    luaunit.assertEquals(read_packet.characteristics_ids[i],characteristics_ids[i]) 
-  end 
+    binary_packet = {
+        1, 3, 1, 1, 1, --header
+        1, --service_id
+        1, 1 --char_count,char_id
+    }
 
-  print("should_read_packet_for_one_service_and_one_characteristic passed")
+    read_packet = parse_read(binary_packet)
+    luaunit.assertEquals(read_packet.service_id, 1)
+    luaunit.assertEquals(read_packet.characteristic_count, 1)
+
+    characteristics_ids = { 1 };
+
+    for i = 1, read_packet.characteristic_count do
+        luaunit.assertEquals(read_packet.characteristics_ids[i], characteristics_ids[i])
+    end
+
+    print("should_read_packet_for_one_service_and_one_characteristic passed")
 end
+
 should_read_packet_for_one_service_and_one_characteristic()
 
-
-
 function should_read_packet_for_one_service_and_two_characteristics()
-  binary_packet = {
-    1, 3, 1, 1, 1,  --header
-    4,              --service_id
-    2, 2, 1            --char_count,char_ids...
-  }
-  
-  read_packet = parse_read(binary_packet)
-  luaunit.assertEquals(read_packet.service_id,3)
-  luaunit.assertEquals(read_packet.characteristic_count,2)
-  
-  characteristics_ids = {2, 1};
-  
-  for i = 1, read_packet.characteristic_count do
-    luaunit.assertEquals(read_packet.characteristics_ids[i],characteristics_ids[i]) 
-  end 
+    binary_packet = {
+        1, 3, 1, 1, 1, --header
+        4, --service_id
+        2, 2, 1 --char_count,char_ids...
+    }
 
-  print("should_read_packet_for_one_service_and_two_characteristics passed")
+    read_packet = parse_read(binary_packet)
+    luaunit.assertEquals(read_packet.service_id, 4)
+    luaunit.assertEquals(read_packet.characteristic_count, 2)
+
+    characteristics_ids = { 2, 1 };
+
+    for i = 1, read_packet.characteristic_count do
+        luaunit.assertEquals(read_packet.characteristics_ids[i], characteristics_ids[i])
+    end
+
+    print("should_read_packet_for_one_service_and_two_characteristics passed")
+end
+
 end
 should_read_packet_for_one_service_and_two_characteristics()
