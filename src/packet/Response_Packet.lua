42883a21a71a510e2f544fa45753108edfcb4961
nurmil <nurmil@thoughtworks.com>
2017-05-12 15:13:16 +0530

    [000] + Urmil/Varun | Stringify response for one characteristic data

--- src/packet/Response_Packet.lua
+++ src/packet/Response_Packet.lua
@@ -1,3 +1,5 @@
+main = require('./src/packet/Characteristic')
+
 -- GLOBAL VARIABLES
 BINARY_PACKET_HEADER_BYTE_SIZE = 7
 
@@ -5,12 +7,13 @@ BINARY_PACKET_HEADER_BYTE_SIZE = 7
 Response_Packet = { service_id = -1, characteristic_count = -1, characteristics = {} }
 
 -- Derived class method new
-function Response_Packet:new(o, service_id, characteristic_count)
+function Response_Packet:new(o, service_id, characteristic_count, characteristics)
     o = o or {}
     setmetatable(o, self)
     self.__index = self
     self.service_id = service_id or -1
     self.characteristic_count = characteristic_count or -1
+    self.characteristics = characteristics or {}
     return o
 end
 
@@ -18,7 +21,7 @@ function stringify_response(response_packet)
     characteristics_size = 0
 
     for index = 1, response_packet.characteristic_count do
-        characteristics_size = characteristics_size + response_packet.characteristic[index].size
+        characteristics_size = characteristics_size + response_packet.characteristics[index].size
     end
 
     binary_packet = {}
@@ -30,6 +33,25 @@ function stringify_response(response_packet)
     binary_packet[5] = 1; --Unused Header Byte
     binary_packet[6] = response_packet.service_id + 1;
     binary_packet[7] = response_packet.characteristic_count;
+
+    last_written_index = BINARY_PACKET_HEADER_BYTE_SIZE;
+
+    for index = 1, response_packet.characteristic_count do
+        last_written_index = last_written_index + 1
+        binary_packet[last_written_index] = response_packet.characteristics[index].id
+        last_written_index = last_written_index + 1
+        binary_packet[last_written_index] = response_packet.characteristics[index].size
+
+        for characteristic_index = 1, response_packet.characteristics[index].size do
+            last_written_index = last_written_index + 1
+            binary_packet[last_written_index] = response_packet.characteristics[index].data[characteristic_index]
+        end
+    end
+
+    if response_packet.characteristic_count > 0 then
+        last_written_index = last_written_index + 1
+        binary_packet[last_written_index] = 0
+    end
 
     return binary_packet
 end
--- tests/packet/Response_Packet_Test.lua
+++ tests/packet/Response_Packet_Test.lua
@@ -17,3 +17,26 @@ function should_stringify_response_having_zero_characteristic_data()
     print("should_stringify_response_having_zero_characteristic_data passed")
 end
 should_stringify_response_having_zero_characteristic_data()
+
+function should_stringify_response_having_one_characteristic_data()
+    characteristic_data = { 1 }
+    characteristic = Characteristic:new { id = 1, size = 1, data = characteristic_data }
+    characteristic_array = { characteristic }
+
+    response_packet = Response_Packet:new { service_id = 0, characteristic_count = 1, characteristics = characteristic_array }
+    response_binary_packet = stringify_response(response_packet)
+
+    expected_response_binary_packet = {
+        1, --Version
+        4, -- ResponsePacketType
+        1, 1, 1, --Unused
+        1, --serviceId
+        1, --charsCount
+        1, 1, 1, --Characteristic id,size,data
+        0 -- Ending string
+    }
+
+    luaunit.assertEquals(response_binary_packet, expected_response_binary_packet)
+    print("should_stringify_response_having_one_characteristic_data passed")
+end
+should_stringify_response_having_one_characteristic_data()
