a96472a211fa82b1c0445808b61aa5eee94562b5
svarun <svarun@varunrocks6.local>
2017-05-12 14:20:19 +0530

    [000] + Urmil/Varun | Removed redundant statements

--- src/packet/Response_Packet.lua
+++ src/packet/Response_Packet.lua
@@ -1,21 +1,3 @@
--- Keeping uninitialized/default values of id and size as -1
-
--- Meta class
-Characteristic = { id = -1, size = -1, data = {} }
-
--- Derived class method new
-
-function Characteristic:new(o, id, size, data)
-    o = o or {}
-    setmetatable(o, self)
-    self.__index = self
-    self.id = id or -1
-    self.size = size or -1
-    self.data = data or {}
-    return o
-end
-
-
 -- GLOBAL VARIABLES
 BINARY_PACKET_HEADER_BYTE_SIZE = 7
 
