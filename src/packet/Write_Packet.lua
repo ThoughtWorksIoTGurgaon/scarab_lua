7e7b86863a817cba26d909706908a801e1163099
nurmil <nurmil@thoughtworks.com>
2017-05-15 15:54:43 +0530

    [000] + Urmil | Create Write_Packet from binary_packet sent by Queenbee for one service and one characteristic

--- src/packet/Write_Packet.lua
+++ src/packet/Write_Packet.lua
@@ -1,4 +1,6 @@
 -- Keeping uninitialized/default values of service_id and characteristic_count as -1
+main = require('./src/packet/Characteristic')
+
 -- GLOBAL VARIABLES
 SERVICE_ID_INDEX = 6 -- INDICES IN LUA START FROM 1
 CHARACTERISTICS_COUNT_INDEX = 7 -- DO I NEED TO REPEAT, IT STARTS FROM 1
@@ -23,5 +25,22 @@ function parse_write(binary_packet)
     service_id = binary_packet[SERVICE_ID_INDEX];
     characteristic_count = binary_packet[CHARACTERISTICS_COUNT_INDEX];
     characteristics = {}
+
+    last_written_index = CHARACTERISTICS_COUNT_INDEX
+
+    for index = 1, characteristic_count do
+        last_written_index = last_written_index + 1
+        id = binary_packet[last_written_index]
+        last_written_index = last_written_index + 1
+        size = binary_packet[last_written_index]
+        data = {}
+
+        for i = 1, size do
+            last_written_index = last_written_index + 1
+            data[i] = binary_packet[last_written_index]
+        end
+        characteristics[index] = Characteristic:new { id = id, size = size, data = data }
+    end
+
     return Write_Packet:new { service_id = service_id, characteristic_count = characteristic_count, characteristics = characteristics }
 end
--- tests/packet/Write_Packet_Test.lua
+++ tests/packet/Write_Packet_Test.lua
@@ -15,3 +15,30 @@ function should_parse_write_packet_for_one_service_and_zero_characteristic()
     print("should_parse_write_packet_for_one_service_and_zero_characteristic passed")
 end
 should_parse_write_packet_for_one_service_and_zero_characteristic()
+
+function should_parse_write_packet_for_one_service_and_one_characteristic()
+    binary_packet = {
+        1, 3, 1, 1, 1, --header
+        1, --service_id
+        1, --char_count
+        1, 1, 1, --Characteristic id,size,data
+        0 -- Ending string
+    }
+
+    write_packet = parse_write(binary_packet)
+    luaunit.assertEquals(write_packet.service_id, 1)
+    luaunit.assertEquals(write_packet.characteristic_count, 1)
+
+    characteristic_data = { 1 }
+    characteristic = Characteristic:new { id = 1, size = 1, data = characteristic_data }
+    characteristic_array = { characteristic }
+
+    write_packet = Write_Packet:new { service_id = 0, characteristic_count = 1, characteristics = characteristic_array }
+
+    for i = 1, write_packet.characteristic_count do
+        luaunit.assertEquals(write_packet.characteristics[i], characteristic_array[i])
+    end
+
+    print("should_parse_write_packet_for_one_service_and_one_characteristic passed")
+end
+should_parse_write_packet_for_one_service_and_one_characteristic()
