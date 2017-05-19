ecef7704af61f97b83cb0962b888f6adea73c16f
nurmil <nurmil@thoughtworks.com>
2017-05-19 11:32:24 +0530

    [000] + Varun/Urmil | Set default status of ports to OFF

--- src/port/Port.lua
+++ src/port/Port.lua
@@ -8,6 +8,6 @@ function Port:new(o, pin, toggle)
     setmetatable(o, self)
     self.__index = self
     self.pin = pin or -1
-    self.toggle = toggle or -1
+    self.toggle = toggle or OFF
     return o
 end
--- tests/port/Port_Test.lua
+++ tests/port/Port_Test.lua
@@ -15,3 +15,14 @@ function should_read_data_from_port()
 end
 should_read_data_from_port()
 
+function should_read_deafaut_toggle_as_OFF_from_port()
+
+    testable_port = Port:new { pin = 5 }
+
+    expected_toggle = OFF
+    actual_toggle = testable_port.toggle
+
+    luaunit.assertEquals(actual_toggle, expected_toggle)
+    print(GREEN.."should_read_data_from_port passed")
+end
+should_read_deafaut_toggle_as_OFF_from_port()
