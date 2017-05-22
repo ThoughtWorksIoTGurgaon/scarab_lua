24c8086f404d53cd058480488460547f49d7d69d
nurmil <nurmil@thoughtworks.com>
2017-05-22 11:51:22 +0530

    [000] + Varun/Urmil | Invoke read function on device with callback

--- src/device/Device.lua
+++ src/device/Device.lua
@@ -8,16 +8,30 @@ function Device:new(o, ports)
     return o
 end
 
+-- Get user given pin from the list of ports
+-- Invoke callback function with pin and toggle
+-- Update appropriate toggle state of port from the callback
+
 function Device:write(pin, toggle, callback)
-    -- Get user given pin from the list of ports
-    -- Invoke callback function with pin and toggle
-    -- Update appropriate toggle state of port from the callback
 
     for unused_port_index, port in pairs(self.ports) do
         if port.pin == pin then
             status = callback(pin, toggle)
             if toggle == status then
+                port.toggle = toggle
                 return true
+            end
+        end
+    end
+    return false
+end
+
+function Device:read(pin, callback)
+
+    for unused_port_index, port in pairs(self.ports) do
+        if port.pin == pin then
+            if callback(pin) then
+                return port.toggle
             end
         end
     end
--- tests/device/Device_Test.lua
+++ tests/device/Device_Test.lua
@@ -52,14 +52,36 @@ function test_write_callback(pin, toggle)
 end
 
 function should_invoke_callback_write_function()
-    usable_ports = { Port:new { pin = 1 }, Port:new { pin = 8, toggle = ON }, Port:new { pin = 3 } }
+    usable_ports = { Port:new { pin = 1 }}
     testable_device = Device:new { ports = usable_ports }
 
-    test_pin=1
-    test_pin_toggle=ON
+    test_pin = 1
+    test_pin_toggle = ON
 
     testable_device:write(test_pin, test_pin_toggle, test_write_callback)
     luaunit.assertTrue(callback_invoked)
+    luaunit.assertEquals(testable_device.ports[1].toggle, test_pin_toggle)
     print(GREEN .. "should_invoke_callback_write_function passed")
 end
 should_invoke_callback_write_function()
+
+callback_invoked = false
+function test_read_callback(pin)
+    callback_invoked = true
+    return true
+end
+
+function should_invoke_callback_read_function()
+    usable_ports = { Port:new { pin = 1 }}
+    testable_device = Device:new { ports = usable_ports }
+
+    test_pin = 1
+
+    expected_toggle = testable_device:read(test_pin, test_read_callback)
+    luaunit.assertTrue(callback_invoked)
+    luaunit.assertEquals(expected_toggle, OFF)
+    luaunit.assertEquals(testable_device.ports[1].toggle, OFF)
+
+    print(GREEN .. "should_invoke_callback_read_function passed")
+end
+should_invoke_callback_read_function()
